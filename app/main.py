0a801177cc1086533b428ee5a31b0f04721415d7
Andrew HoChoy <a.hochoy@gmail.com>
2025-03-17 18:05:37 -0700

    Adding some styling and branding

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-from nicegui import ui
+from nicegui import ui, html
 from ai_foodie_planner import get_food_tour
+from assets.logo import lft_logo
 
 @ui.page('/')
 def main():
     async def submit_form(character, cuisine):
 
         tour_container.clear()
+        tour_container.set_visibility(True)
 
         if (character.error or cuisine.error):
             with tour_container:
@@ -25,26 +27,36 @@ def main():
     cuisine_options = ['Italian', 'Mexican', 'Chinese', 'Japanese', 'Indian', 'Thai', 'French', 'Greek', 'Mediterranean', 'American', 'Korean', 'Southern', 'BBQ']
 
     ui.query('.q-page').classes('flex')
-    ui.query('.nicegui-content').classes('w-full max-w-5xl mx-auto p-4')
+    ui.query('.nicegui-content').classes('w-full')
 
-    ui.label('Literary Food Tour').classes('w-full text-2xl font-bold mt-4')
 
-    with ui.grid(columns='1fr 1fr fit-content(10%)').classes('w-full'):
-        character = ui.input(
-            placeholder='Enter a character name',
-            validation=lambda value: 'Character name too short' if len(value) < 3 else None,
-        ).props('rounded outlined dense')
-        cuisine = ui.select(
-            options=cuisine_options,
-            value=cuisine_options[0],
-            with_input=True, 
-            new_value_mode='add-unique',
-            validation=lambda value: 'Cuisine should not be empty' if value is None else None,
-        ).props('rounded outlined dense')
-        ui.button(
-            'Let\'s Eat!', on_click=lambda: submit_form(character, cuisine)
-        ).props('rounded outline').classes('self-start').bind_enabled_from(character, 'value')
+    with html.header():
+        lft_logo().classes('max-w-xs')
 
-    tour_container = ui.column().classes('w-full mt-4')
+    with html.section().classes('w-full max-w-4xl mx-auto text-center p-10 border border-stone-400 bg-stone-50 rounded-2xl'):
+        ui.label('Literary Food Tour').classes('w-full text-2xl font-bold mt-4')
+        ui.label('Plan a food tour for a literary character and decide what type of food you will eat.').classes('w-full text-lg mt-2')
+
+        with ui.row().classes('flex pt-10'):
+            character = ui.input(
+                placeholder='Enter a character name',
+                validation=lambda value: 'Character name too short' if len(value) < 3 else None,
+            ).props('rounded outlined dense bg-color="white"').classes('grow')
+            cuisine = ui.select(
+                options=cuisine_options,
+                value=cuisine_options[0],
+                with_input=True, 
+                new_value_mode='add-unique',
+                validation=lambda value: 'Cuisine should not be empty' if value is None else None,
+            ).props('rounded outlined dense bg-color="white"').classes('grow')
+            ui.button(
+                'Let\'s Eat!', on_click=lambda: submit_form(character, cuisine)
+            ).props('rounded outline').classes('self-start').bind_enabled_from(character, 'value')
+
+    tour_container = ui.column().classes('w-full mt-4 max-w-4xl mx-auto p-10 border border-stone-400 bg-stone-50 rounded-2xl')
+    tour_container.set_visibility(False)
+
+    with html.footer().classes('mt-auto'):
+        ui.label('Made with Love - An Experiment by Andrew').classes('text-xs text-stone-400')
 
 ui.run(port=80, title='Literary Food Tour')
